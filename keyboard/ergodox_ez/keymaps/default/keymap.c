232eb92752a502d12243dc34cbb74b0565108a82
平石 陽介 <hiraishi_yosuke@hiraishi-no-MacBook-Air.local>
2016-05-21 22:40:53 +0900

    my keymap initial commit

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -6,49 +6,51 @@
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
 
+#define C_S(kc)   kc | 0x0100 | 0x0200 // `Ctrl + Shift + kc`(= `kc | 0x1400`)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+ * | Grv    |   1  |   2  |   3  |   4  |   5  |  ~   |           |  ^   |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Del    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  |   |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |--------+------+------+------+------+------|      |           |  _   |------+------+------+------+------+--------|
+ * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   | ~L1  |  '"  |AltShf| LAlt | EISU |                                       | KANA | RAlt |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
- *                                 `--------------------'       `----------------------'
+ *                                  ,-------------------.       ,------------------.
+ *                                  |   LEFT  |  RIGHT  |       | MsLeft | MsRight |
+ *                           ,------|---------|---------|       |--------+---------+-------.
+ *                           |      |         |    UP   |       |  MsUp  |         |       |
+ *                           | Space|Backspace|---------|       |--------|   Esc   | Enter |
+ *                           |      |         |   DOWN  |       | MsDown |         |       |
+ *                            `-------------------------'       `--------------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_DELT,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
-        KC_BSPC,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
-                                              ALT_T(KC_APP),  KC_LGUI,
-                                                              KC_HOME,
-                                               KC_SPC,KC_BSPC,KC_END,
+        KC_GRV,           KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_TILD,
+        KC_TAB,           KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_PIPE,
+        KC_LCTRL,         KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LSFT,          KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_TRNS,
+        MO(SYMB),         KC_QUOT,      LALT(KC_LSFT),  KC_LALT,C_S(KC_SCLN),
+                                                                KC_LEFT,KC_RIGHT,
+                                                                           KC_UP,
+                                                          KC_SPC,KC_BSPC,KC_DOWN,
         // right hand
-             KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
-             TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
-             MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
-             KC_LALT,        CTL_T(KC_ESC),
-             KC_PGUP,
-             KC_PGDN,KC_TAB, KC_ENT
+        KC_CIRC,     KC_6,   KC_7,         KC_8,   KC_9,   KC_0,             KC_MINS,
+        KC_UNDS,     KC_Y,   KC_U,         KC_I,   KC_O,   KC_P,             KC_BSLS,
+                     KC_H,   KC_J,         KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
+        KC_TRNS,     KC_N,   KC_M,         KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+                             C_S(KC_J), KC_RALT,KC_LBRC,KC_RBRC,          KC_FN1,
+        KC_MS_L,        KC_MS_R,
+        KC_MS_U,
+        KC_MS_D,KC_ESC, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
