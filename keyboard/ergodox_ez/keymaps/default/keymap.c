676cb3d0c951871a4be2e52c96afb7f97cf2c4de
hiraishi_yosuke <yosuke.hiraishi@drecom.co.jp>
2016-06-07 14:06:46 +0900

    Change Alt+Shift to Cmd

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Lclk |           | Rclk |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | ~L1  | Cmd  |AltShf| LAlt | EISU |                                       | KANA | RAlt |      |      | ~L1  |
+ *   | ~L1  |AltShf| Cmd  | LAlt | EISU |                                       | KANA | RAlt |      |      | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                  ,-------------------.       ,--------------.
  *                                  |  MsLeft | MsRight |       | LEFT | RIGHT |
@@ -35,11 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_GRV,           KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_TRNS,
-        KC_TAB,           KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_TRNS,
-        KC_LCTRL,         KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,          KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_BTN1,
-        MO(SYMB),         KC_LGUI,      LALT(KC_LSFT),  KC_LALT,C_S(KC_SCLN),
+        KC_GRV,           KC_1,            KC_2,     KC_3,   KC_4,   KC_5,   KC_TRNS,
+        KC_TAB,           KC_Q,            KC_W,     KC_E,   KC_R,   KC_T,   KC_TRNS,
+        KC_LCTRL,         KC_A,            KC_S,     KC_D,   KC_F,   KC_G,
+        KC_LSFT,          KC_Z,            KC_X,     KC_C,   KC_V,   KC_B,   KC_BTN1,
+        MO(SYMB),         LALT(KC_LSFT),   KC_LGUI,  KC_LALT,C_S(KC_SCLN),
                                                                 KC_MS_L,KC_MS_R,
                                                                         KC_MS_U,
                                                          KC_SPC,KC_BSPC,KC_MS_D,
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,     KC_6,   KC_7,         KC_8,   KC_9,   KC_0,             KC_MINS,
         KC_TRNS,     KC_Y,   KC_U,         KC_I,   KC_O,   KC_P,             KC_BSLS,
                      KC_H,   KC_J,         KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_COLN,
-        KC_BTN2,    KC_N,   KC_M,         KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+        KC_BTN2,     KC_N,   KC_M,         KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
                              C_S(KC_J), KC_RALT,KC_TRNS,KC_TRNS,            MO(SYMB),
         KC_LEFT,     KC_RIGHT,
         KC_UP,
