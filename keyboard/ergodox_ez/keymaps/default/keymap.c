758ee5e4dbe8ab18deacd816a93a3662dfaa1c93
hiraishi_yosuke <yosuke.hiraishi@drecom.co.jp>
2017-07-13 14:56:09 +0900

    Change KC_GRV to KANA

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | Grv    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
+ * | KANA   |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_GRV,           KC_1,            KC_2,     KC_3,   KC_4,   KC_5,   KC_TRNS,
+        C_S(KC_J),        KC_1,            KC_2,     KC_3,   KC_4,   KC_5,   KC_TRNS,
         KC_TAB,           KC_Q,            KC_W,     KC_E,   KC_R,   KC_T,   KC_TRNS,
         KC_LCTRL,         KC_A,            KC_S,     KC_D,   KC_F,   KC_G,
         KC_LSFT,          KC_Z,            KC_X,     KC_C,   KC_V,   KC_B,   KC_BTN1,
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,     KC_Y,   KC_U,         KC_I,   KC_O,   KC_P,             KC_BSLS,
                      KC_H,   KC_J,         KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_COLN,
         KC_BTN2,     KC_N,   KC_M,         KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                             C_S(KC_J), KC_RALT,KC_TRNS,KC_TRNS,            MO(SYMB),
+                             KC_GRV, KC_RALT,KC_TRNS,KC_TRNS,            MO(SYMB),
         KC_LEFT,     KC_RIGHT,
         KC_UP,
         KC_DOWN,KC_ESC, KC_ENT
